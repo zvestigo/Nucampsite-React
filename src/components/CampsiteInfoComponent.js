ed80a84c86c25ee2b2c4e3c60956b9bcc214f802
zvestigo <zvestigo@icloud.com>
2021-02-04 12:24:12 -0500

    Functional Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,64 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
 
-    renderCampsite(campsite) {              // Prints Details for Site from props
+
+function RenderCampsite({campsite}) {              // Prints Details for Site from props
+    return(
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+
+function RenderComments({comments}) {              // Prints Comments for Site
+    if (comments) {
         return(
             <div className="col-md-5 m-1">
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
+                <h4>Comments</h4>
+                {comments.map(comment => {      // Prints all comments
+                    return(
+                        <div key={comment.id}> 
+                            <p>{comment.text} <br />
+                            -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                        </div>
+                    );
+                })}
             </div>
         );
-    }
-
-    renderComments(comments) {              // Prints Comments for Site
-        if (comments) {
-            return(
-                <div className="col-md-5 m-1">
-                    <h4>Comments</h4>
-                    {comments.map(comment => {      // Prints all comments
-                        return(
-                            <div key={comment.id}> 
-                                <p>{comment.text} <br />
-                                -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                            </div>
-                        );
-                    })}
-                </div>
-            );
-        } else {
-            return <div />;
-        }
-    }
-
-    render(){                               // Displays panel w/ details & Comments
-        const {campsite} = this.props;
-
-        if (campsite) {         // Evaluates if campsite is selected
-            return(
-                <div className="container">
-                    <div className="row">
-                        {this.renderCampsite(campsite)}
-                        {this.renderComments(campsite.comments)}
-                    </div>
-                </div>
-                );
-        } else {                // Runs only if campsite is null
-            return(
-                <div />
-            );
-        }
+    } else {
+        return <div />;
     }
 }
+
+function CampsiteInfo(props){                               // Displays panel w/ details & Comments
+    if (props.campsite) {         // Evaluates if campsite is selected
+        return(
+            <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.campsite.comments} />
+                </div>
+            </div>
+        );
+    }                
+    return <div />;     // Runs only if campsite is null
+}
+
 
 export default CampsiteInfo;
