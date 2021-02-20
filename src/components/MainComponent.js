f2ffa97c7b170b09f700e089bc97a70a34926ea5
zvestigo <zvestigo@icloud.com>
2021-02-20 11:23:30 -0500

    Resolving warnings

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
